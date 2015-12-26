8a1b545b29fb2df3061a729390f8ecd3a1d0185c
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2015-12-26 15:11:34 -0200

    Authentication and Retrieve items

--- src/EasyPocket.UWP.UI/MainPage.xaml.cs
+++ src/EasyPocket.UWP.UI/MainPage.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,101 @@ namespace EasyPocket.UWP.UI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const string consumerKey = "49510-2b106efad3cb48ae12eab7f9";
+        private static string accessToken;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public async Task Auth()
+        {
+            var callback = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
+            string code;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+
+                var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("consumer_key", consumerKey),
+                new KeyValuePair<string, string>("redirect_uri", callback)
+            });
+
+                var postResponse = await httpClient.PostAsync("https://getpocket.com/v3/oauth/request", content);
+                var contentResponse = await postResponse.Content.ReadAsStringAsync();
+
+                code = contentResponse.Substring(5);
+            }
+
+
+
+            Uri StartUri = new Uri($"https://getpocket.com/auth/authorize?request_token={code}&redirect_uri={callback}&webauthenticationbroker=1");
+            Uri EndUri = new Uri(callback);
+
+
+#if WINDOWS_PHONE_APP
+                WebAuthenticationBroker.AuthenticateAndContinue(StartUri, EndUri, null, WebAuthenticationOptions.None);
+#else
+            WebAuthenticationResult WebAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, StartUri);
+            if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+            {
+                string token = WebAuthenticationResult.ResponseData.ToString();
+
+                using (HttpClient httpClient = new HttpClient())
+                {
+
+                    var content = new FormUrlEncodedContent(new[]
+                {
+                new KeyValuePair<string, string>("consumer_key", consumerKey),
+                new KeyValuePair<string, string>("code", code)
+            });
+
+                    var postResponse = await httpClient.PostAsync("https://getpocket.com/v3/oauth/authorize", content);
+                    var contentResponse = await postResponse.Content.ReadAsStringAsync();
+
+                    var resultValues = ParseQueryString(contentResponse);
+
+                    accessToken = resultValues["access_token"];
+                    var username = resultValues["username"];
+                }
+            }
+            else if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+            {
+                //Console.WriteLine("HTTP Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseErrorDetail.ToString());
+            }
+            else
+            {
+                // OutputToken("Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseStatus.ToString());
+            }
+#endif
+        }
+
+        public static Dictionary<string, string> ParseQueryString(string query)
+        {
+            WwwFormUrlDecoder decoder = new WwwFormUrlDecoder(query);
+            return decoder.ToDictionary(x => x.Name, x => x.Value);
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Auth();
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+
+                var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("consumer_key", consumerKey),
+                new KeyValuePair<string, string>("access_token", accessToken)
+            });
+
+                var postResponse = await httpClient.PostAsync("https://getpocket.com/v3/get", content);
+                var contentResponse = await postResponse.Content.ReadAsStringAsync();
+
+                
+            }
         }
     }
 }
